af76ccd5cfb0398c53abb54ae09654bbab90cf94
Jake Austin <jakeaust07@gmail.com>
2014-08-18 17:13:34 -0400

    update project flow notes

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ $(document).ready(function() {
   var refs = [];
   var resps = [];
 
-  // have some animation play on home screen ---------
+  $('#userResponse').hide();
+  $('#countdown').hide();
+
+
+/////////////////////////////////////////////////////////////////////
+  // have some animation play on home screen --------- no timeout, repeat...
+  // self-invoking function to perform animation?
 
   var itiContainer = d3.select('#svg-iti')
   .attr("height", '100%')
@@ -12,23 +18,21 @@ $(document).ready(function() {
   // Need
   // 1) cartesian plane
   // 2) x and y scale (x changes based on resp.length, y on resp.min & resp.max)
+  // 3) x and y axes (tap # and time in ms)
 
-var rpContainer = d3.select("#svg-rp")
-.attr("height", '100%')
-.attr("width", '100%');
+  var rpContainer = d3.select("#svg-rp")
+  .attr("height", '100%')
+  .attr("width", '100%');
 
-var circle = rpContainer.append("circle")
-.attr("cx", '50%')
-.attr("cy", '50%')
-.attr("r", '30%');
-  // Need
-  // 1) plot line function (takes in angle)
+  var circle = rpContainer.append("circle")
+  .attr("cx", '50%')
+  .attr("cy", '50%')
+  .attr("r", '30%');
+    // Need
+    // 1) plot line function (takes in angle)
+//////////////////////////////////////////////////////////////////////
 
-
-$('#userResponse').hide();
-$('#countdown').hide();
-$('#start-button').click(runDemo);
-
+  $('#start-button').click(runDemo);
 
 });
 
@@ -39,6 +43,7 @@ var runDemo = function() {
 
   // use modeSelect to hide SVG charts if 'challenge'
   var modeSelect = $('input[name="mode"]').val();
+
   // use difficultySelect to determine which audio to serve
   var difficultySelect = $('input[name="difficulty"]').val();
 
@@ -56,14 +61,25 @@ var runDemo = function() {
   counter();
 
   // 2) serve audio
-  // 3) track user responses, store into resps
-  // 4) do math with user response --> update SVG charts
-  // *****
-  // 5) end of audio --> STOP tracking, enter 'review' mode
-  //    --> 'click here' changed to 'done' (returns to 'home screen')
+  //    - ajax get request to get audio file
+  //    - append audio tag to body with src = response, autoplay = true
+  //    - include track tag to get currentTime, duration etc
+  // WHILE currentTime < duration
+  //    3) track user responses, store into resps
+  //      --> what do about skipped taps? Rapid taps?
+  //        --> if ITI < 600 DISREGARD TAP TIME
+  //        --> if ITI > 1000 AWARD PERFECT TAP
+  //        --> OR DO NOTHING!
+  //    4) do math with user response
+  //  *****
+  //  4.5) update SVG charts
+  //  *****
+  // currentTime >= duration
+  //  5) end of audio --> STOP tracking, enter 'review' mode
   //    --> some way to interact with figures
+  //    --> 'click here' changed to 'done' (hide/shows, play animation)
 
-  //debugger;
+  debugger;
 };
 
 var counter = function() {
