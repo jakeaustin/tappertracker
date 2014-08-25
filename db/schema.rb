10a38d3c184f6ed8e0b04a293dc126e512fbf500
Jake Austin <jakeaust07@gmail.com>
2014-08-25 16:40:51 -0400

    default user scores

--- db/schema.rb
+++ db/schema.rb
@@ -17,21 +17,21 @@ ActiveRecord::Schema.define(version: 20140825193941) do
   enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
-    t.string   "email",                                          default: "", null: false
-    t.string   "encrypted_password",                             default: "", null: false
+    t.string   "email",                                          default: "",     null: false
+    t.string   "encrypted_password",                             default: "",     null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                                  default: 0,  null: false
+    t.integer  "sign_in_count",                                  default: 0,      null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.decimal  "slow_score",             precision: 5, scale: 2
-    t.decimal  "medium_score",           precision: 5, scale: 2
-    t.decimal  "fast_score",             precision: 5, scale: 2
+    t.decimal  "slow_score",             precision: 6, scale: 2, default: 1000.0
+    t.decimal  "medium_score",           precision: 6, scale: 2, default: 1000.0
+    t.decimal  "fast_score",             precision: 6, scale: 2, default: 1000.0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
