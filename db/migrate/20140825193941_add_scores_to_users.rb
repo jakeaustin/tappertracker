67924756be083b983476ae7b63f7235737cf1ea7
Jake Austin <jakeaust07@gmail.com>
2023-01-11 16:10:08 -0500

    update/migrate db

--- db/migrate/20140825193941_add_scores_to_users.rb
+++ db/migrate/20140825193941_add_scores_to_users.rb
@@ -1,4 +1,4 @@
-class AddScoresToUsers < ActiveRecord::Migration
+class AddScoresToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :slow_score, :decimal, precision: 6, scale: 2, default: 1000
     add_column :users, :medium_score, :decimal, precision: 6, scale: 2, default: 1000
